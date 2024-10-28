c9e9425acecbb8de1efa9fb887f109446b6f4230
Ayush <ayu11310@gmail.com>
2024-10-29 01:48:57 +0530

    made changes in card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,66 +1,65 @@
 import React from 'react';
+import { IoMdPeople } from "react-icons/io";
+import { BsSpeedometer } from "react-icons/bs";
+import { FaTools } from "react-icons/fa";
+import { SiGoogleearthengine } from "react-icons/si";
+
 
 function Card({ data }) {
     return (
         <div className="bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 m-4">
             <img className="w-full h-48 object-cover" src={data.image} alt={data.title} />
-            <div className="p-4">
-                <h3 className="text-xl font-semibold text-gray-800">{data.title}</h3>
-                <p className="text-gray-600">{data.year}</p>
+            <div className="p-4 flex  justify-between">
+                <div>
+                    <h3 className="text-xl font-semibold text-gray-800">{data.title}</h3>
+                    <p className="text-gray-600">{data.year}</p>
+                </div>
+
+                {/* <div className="text-right">
+                    <h2 className="text-2xl font-bold text-gray-800">${data.price}</h2>
+                    <p className="text-gray-600">/month</p>
+                </div> */}
             </div>
+
+
+
             <div className="px-4 py-2 bg-gray-100">
+{/* 
+                <ul>
+                    <li className="flex items-center gap-2 text-gray-700"> <IoMdPeople className='text-xl' />
+                        {data.people} People</li>
+                    <li className="flex items-center gap-2 text-gray-700"> <SiGoogleearthengine className='text-xl' />
+                        {data.type}</li>
+                    <li className="flex items-center gap-2 text-gray-700"> <BsSpeedometer className='text-lg' />{data.Speed}</li>
+                    <li className="flex items-center gap-2 text-gray-700"> <FaTools className='text-lg' />
+                        {data.mode}</li>
+                </ul> */}
+
+
+
                 <div className="flex justify-between mb-2">
                     <span className="flex items-center">
-                        <h5 className="flex items-center text-gray-700">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="1em"
-                                className="mr-1 w-5 h-5"
-                                viewBox="0 0 640 512"
-                            >
-                                <path d="M96 128...z" /> {/* SVG code */}
-                            </svg>
+                        <h5 className="flex items-center gap-2 text-gray-700">
+                            <IoMdPeople className='text-xl' />
                             {data.people} People
                         </h5>
                     </span>
                     <span className="flex items-center">
-                        <h5 className="flex items-center text-gray-700">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="1em"
-                                className="mr-1 w-5 h-5"
-                                viewBox="0 0 512 512"
-                            >
-                                <path d="M32 64...z" /> {/* SVG code */}
-                            </svg>
+                        <h5 className="flex items-center gap-2 text-gray-700">
+                            <SiGoogleearthengine className='text-xl' />
                             {data.type}
                         </h5>
                     </span>
                 </div>
                 <div className="flex justify-between">
                     <span className="flex items-center">
-                        <h5 className="flex items-center text-gray-700">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="1em"
-                                className="mr-1 w-5 h-5"
-                                viewBox="0 0 512 512"
-                            >
-                                <path d="M0 256...z" /> {/* SVG code */}
-                            </svg>
-                            {data.Speed}
-                        </h5>
+                        <h5 className="flex items-center gap-2 text-gray-700">
+                            <BsSpeedometer className='text-lg' />{data.Speed}</h5>
                     </span>
                     <span className="flex items-center">
-                        <h5 className="flex items-center text-gray-700">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="1em"
-                                className="mr-1 w-5 h-5"
-                                viewBox="0 0 512 512"
-                            >
-                                <path d="M495.9 166.6...z" /> {/* SVG code */}
-                            </svg>
+                        <h5 className="flex items-center gap-2 text-gray-700">
+
+                            <FaTools className='text-lg' />
                             {data.mode}
                         </h5>
                     </span>
